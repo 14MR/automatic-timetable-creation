067f86ab7c70a56507e35553a9a8aa7e3324ec8d
Vladik <qvviko@gmail.com>
2019-03-14 21:41:05 +0300

    + models

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class RoomType(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Rooms(models.Model):
+    number = models.IntegerField()
+    type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
+    capacity = models.IntegerField()
+    is_yellow = models.BooleanField(default=False)
+
+
+class ItemType(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
