b57c11b7bf73f48f2fdc4ec6b5145026193641c8
Vladik <qvviko@gmail.com>
2019-03-14 23:04:06 +0300

    + field items to the room

--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class ItemType(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+
+
 class RoomType(models.Model):
     name = models.CharField(max_length=30)
 
@@ -10,12 +19,4 @@ class Room(models.Model):
     type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
     capacity = models.PositiveSmallIntegerField()
     is_yellow = models.BooleanField(default=False)
-
-
-class ItemType(models.Model):
-    name = models.CharField(max_length=30)
-
-
-class Item(models.Model):
-    name = models.CharField(max_length=30)
-    type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Item)
