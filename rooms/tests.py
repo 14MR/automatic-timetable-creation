d4a886f746fac9d0361aca73d2560820b6a91dc9
kamilkoduo <kamilkoduo@gmail.com>
2019-03-15 01:56:10 +0300

    final for today

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from rooms.models import ItemType
 # Create your tests here.
+from django.test import TestCase
+
+
+class Test1(T):
+    """This module has unit tests specified in test cases for first delivery"""
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        it = ItemType()
+        it.name = "test"
+        self.assertEquals(it.name, "test")
--- schedule/tests.py
+++ schedule/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+
+class Test1(TestCase):
+    """This module has unit tests specified in test cases for first delivery"""
+
+    def setUp(self):
+        pass
+
+    def test(self):
+
+           self.assertTrue(True)
