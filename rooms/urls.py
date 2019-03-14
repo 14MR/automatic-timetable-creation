9939251404f0d92208ab9088c2b6ddf3f14a3f2c
Vladik <qvviko@gmail.com>
2019-03-15 00:07:27 +0300

    + rest framework

---
+++ rooms/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf.urls import url, include
+from rooms.models import Room
+from rest_framework import router, serializers, viewsets
+
+urlpatterns = [
+    path(r'', ),
+]
