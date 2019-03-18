241d6f8c1e62cf63494d1f412f054e7fe64308d9
Vladik <qvviko@gmail.com>
2019-03-18 21:21:41 +0300

    + migrate

--- rooms/models.py
+++ rooms/models.py
@@ -7,7 +7,8 @@ class ItemType(models.Model):
 
 class Item(models.Model):
     name = models.CharField(max_length=30)
-    type = models.ForeignKey(ItemType, on_delete=models.CASCADE, blank=True, null=True)
+    type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+    room = models.ForeignKey('Room', on_delete=models.CASCADE, blank=True, null=True)
 
 
 class RoomType(models.Model):
@@ -19,4 +20,3 @@ class Room(models.Model):
     type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
     capacity = models.PositiveSmallIntegerField()
     is_yellow = models.BooleanField(default=False)
-    items = models.ManyToManyField(Item, blank=True)
