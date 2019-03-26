033479ef2beee7dbfd5d8fd49b0a6a8e1b9a73f6
Vladik <qvviko@gmail.com>
2019-03-26 12:00:07 +0300

    + create test

--- users/test_users.py
+++ users/test_users.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from users.models import User
+from users.models import User, YearGroup, Group
 
 user_data = {
     "first_name": "Bob",
@@ -41,3 +41,36 @@ class TestAuth(APITestCase):
         data = {"email": user_data.pop("email"), "password": user_data.pop("password")}
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+group_data = {
+    "number": 3,
+}
+
+year_group_data = {
+    "year": 2017,
+    "type": 1
+}
+
+
+class TestGroups(APITestCase):
+    def setUp(self):
+        self.year_group = YearGroup.objects.create(**year_group_data)
+        group_data['year_id'] = self.year_group.id
+        self.group = Group.objects.create(**group_data)
+
+    # Test GET, POST on /users/year_groups/
+    # Test PUT, DELETE on /users/year_groups/{id}/
+    # Test GET, POST on /users/groups/
+    # Test PUT, DELETE on /users/groups/{id}
+    # Test PUT on /users/{id}/groups/{id}
+    def test_create_and_delete_new_group(self):
+        group_count = Group.objects.count()
+        new_group_data = {
+            "number": 2,
+            "year_id": self.year_group.id
+        }
+        url = "/api/v1/users/groups"
+
+        response = self.client.post(url, new_group_data, format="json")
+        self.assertEqual(group_count + 1, Group.objects.count())
