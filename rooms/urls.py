e8caf561395183071c5b5e3b9b1d9b04df6deeb3
Timur <mr14mr@yandex.ru>
2019-03-26 17:15:22 +0300

    ! router in rooms

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,13 +2,10 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from rooms.views import RoomViewSet, ItemViewSet
 
-room_router = routers.DefaultRouter()
-room_router.register(r"", RoomViewSet, base_name="room")
-
-item_router = routers.DefaultRouter()
-item_router.register(r"items", ItemViewSet, base_name="item")
+router = routers.DefaultRouter()
+router.register(r"items", ItemViewSet, base_name="item")
+router.register(r"", RoomViewSet, base_name="room")
 
 urlpatterns = [
-    url(r"^", include(item_router.urls), name="items"),
-    url(r"^", include(room_router.urls), name="rooms"),
+    url(r"^", include(router.urls), name="items"),
 ]
