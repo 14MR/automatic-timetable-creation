639f144526fed2e33f3d271acc5d98826cbc2ccc
Vladik <qvviko@gmail.com>
2019-03-30 12:04:03 +0300

    - schedule models from views

--- schedule/urls.py
+++ schedule/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import url
 from django.urls import include
 from rest_framework import routers
 
-from schedule.views import ScheduleViewSet, EventViewSet
+from schedule.views import EventViewSet
 
 router = routers.DefaultRouter()
 router.register(r"events", EventViewSet, basename="event")
-router.register(r"", ScheduleViewSet, basename="schedule")
 
 urlpatterns = [
     url(r"^", include(router.urls), name="schedules"),
--- schedule/views.py
+++ schedule/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
-from schedule.serializers import ScheduleSerializer, Schedule, Event, EvenSerializer
-
-
-class ScheduleViewSet(viewsets.ModelViewSet):
-    serializer_class = ScheduleSerializer
-    permission_classes = [AllowAny]
-    queryset = Schedule.objects.all()
+from schedule.serializers import Event, EvenSerializer
 
 
 class EventViewSet(viewsets.ModelViewSet):
