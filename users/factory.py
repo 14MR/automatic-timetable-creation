c76b914fe63487d46067a36098da21fe8d9bc558
akkyma <akkyma232@gmail.com>
2019-04-02 02:57:02 +0300

    User creation test fixed

--- users/factory.py
+++ users/factory.py
@@ -30,5 +30,4 @@ class UserFactory(factory.django.DjangoModelFactory):
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
     group = factory.SubFactory(GroupFactory)
-    is_admin = False
     is_active = True
