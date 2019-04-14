e2f162e6fe1d59e15c31271a745b2bae45136785
Vladik <qvviko@gmail.com>
2019-04-14 18:52:32 +0300

    - removed print

--- atc/paginators.py
+++ atc/paginators.py
@@ -4,5 +4,4 @@ from rest_framework.response import Response
 
 class LimitOffsetSimplePagination(pagination.LimitOffsetPagination):
     def get_paginated_response(self, data):
-        print(dir(self))
         return Response(data, headers={'X-total': self.count})
